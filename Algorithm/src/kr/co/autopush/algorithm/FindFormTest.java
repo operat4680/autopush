55db5d0a5201f6202a90dbf79e1c91fc3ffa56a0
operat4680 <operat4680@gmail.com>
2014-01-09 21:15:41 +0900

    LoginData Bean 추가, FindForm module 수정

--- Algorithm/src/kr/co/autopush/algorithm/FindFormTest.java
+++ Algorithm/src/kr/co/autopush/algorithm/FindFormTest.java
@@ -1,6 +1,8 @@
 package kr.co.autopush.algorithm;
 import java.io.IOException;
 
+import kr.co.autopush.bean.LoginData;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,13 +15,17 @@ public class FindFormTest {
 //	public static String url = "http://home.mju.ac.kr/board/boardList.action?boardId=1991604&target=home&tab=board1991604&siteId=bongbong";
 //	public static String url = "https://sso.mju.ac.kr/swift/login/login_eclass.jsp?RSP=eclass.mju.ac.kr&RelayState=/home/index.action?siteId=bongbong";
 //	public static String url = "https://www.facebook.com/";
-//	public static String url = "http://portal.ajou.ac.kr/public/portalLogin.jsp";
-	public static String url = "http://builder.hufs.ac.kr/user/popupLogin.action?siteId=hufs&returnUrl=/user/indexFrame.action?framePath=div2_row_1.jsp&siteId=hufs&leftPage=&rightPage=08.html";
+//	public static String url = "http://www.daum.net";
+	public static String url = "https://www.tumblr.com/login";
 	
+//	public static String url = "http://portal.ajou.ac.kr/public/portalLogin.jsp";
+//	public static String url = "http://builder.hufs.ac.kr/user/popupLogin.action?siteId=hufs&returnUrl=/user/indexFrame.action?framePath=div2_row_1.jsp&siteId=hufs&leftPage=&rightPage=08.html";
+//	public static String url = "http://www.naver.com";
 	public static void main(String[] args) {
 	
 		FindForm form = new FindForm(url);
-		form.find();
+		LoginData data = form.getLoginData();
+		data.print();
 	}
 
 }
