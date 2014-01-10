0d17041b255a2e2bf1929c66b64363bca92e4674
operat4680 <operat4680@gmail.com>
2014-01-10 15:41:47 +0900

    modify FindForm, Path return 방식 변경

--- Algorithm/src/kr/co/autopush/algorithm/FindFormTest.java
+++ Algorithm/src/kr/co/autopush/algorithm/FindFormTest.java
@@ -15,9 +15,9 @@ public class FindFormTest {
 //	public static String url = "http://home.mju.ac.kr/board/boardList.action?boardId=1991604&target=home&tab=board1991604&siteId=bongbong";
 //	public static String url = "https://sso.mju.ac.kr/swift/login/login_eclass.jsp?RSP=eclass.mju.ac.kr&RelayState=/home/index.action?siteId=bongbong";
 //	public static String url = "https://www.facebook.com/";
-//	public static String url = "http://www.daum.net";
-	public static String url = "https://www.tumblr.com/login";
-	
+	public static String url = "http://www.daum.net";
+//	public static String url = "https://www.tumblr.com/login";
+//	public static String url = "https://github.com/login";
 //	public static String url = "http://portal.ajou.ac.kr/public/portalLogin.jsp";
 //	public static String url = "http://builder.hufs.ac.kr/user/popupLogin.action?siteId=hufs&returnUrl=/user/indexFrame.action?framePath=div2_row_1.jsp&siteId=hufs&leftPage=&rightPage=08.html";
 //	public static String url = "http://www.naver.com";
